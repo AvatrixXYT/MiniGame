db84a212bde0098db8279ed6eeb2417e4fb4c458
AvatrixXYT <82200585+AvatrixXYT@users.noreply.github.com>
2025-04-21 17:24:20 -0600

    Update Main.java

--- src/main/java/com/avatrixxyt/minigame/Main.java
+++ src/main/java/com/avatrixxyt/minigame/Main.java
@@ -4,6 +4,12 @@ import java.util.Random;
 import java.util.Scanner; //lectura del teclado
 import com.avatrixxyt.util.*;
 
-public class Main {
-	
+public class Main 
+{
+	public static void main(String[]args) throws InterruptedException 
+	{
+		Clean ex = new Clean();
+		Scanner tec = new Scanner(System.in);
+		Random grn = new Random(System.currentTimeMillis()); //variable para el numero aleatorio
+	}
 }
